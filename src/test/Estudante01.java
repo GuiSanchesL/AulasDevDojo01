8f928bcf4fe02dbc80b9eb09601a31c641963e63
SeuNome <guilhermesanchesloureiro@gmail.com>
2026-06-12 15:30:53 -0300

    Uso do java.test para dar a classe nome,idade,sexo e altura

---
+++ src/test/Estudante01.java
@@ -0,0 +1,17 @@
+package test;
+
+import Dominio.Estudante;
+
+public class Estudante01 {
+    public static void main(String[] args) {
+        Estudante estudante=new Estudante();
+        estudante.nome="Gabriel";
+        estudante.idade=14;
+        estudante.sexo='M';
+        estudante.altura=1.65;
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante.altura);
+    }
+}
