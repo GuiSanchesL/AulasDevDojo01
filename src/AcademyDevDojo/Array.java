6f1a2f59063fc4ce990d278e8a2fdc93241f7d38
SeuNome <guilhermesanchesloureiro@gmail.com>
2026-07-10 19:30:21 -0300

    Dei uma grande revisada em Arrays e Arrays multidimensionais

---
+++ src/AcademyDevDojo/Array.java
@@ -0,0 +1,25 @@
+package AcademyDevDojo;
+
+import java.util.ArrayList;
+
+public class Array {
+    public static void main(String[] args) {
+        String [] nome= new String[3];
+        nome [1]="Guilhere";
+        System.out.println("=======================");
+        ArrayList<String>nomes=new ArrayList<>();
+        nomes.add("Anderson");
+
+        int [] age=new int[5];
+        age [1] =1;
+        System.out.println(age);
+
+
+        int [] []  idades=new int[0][5];
+
+        idades [5][1]=10;
+        idades [2][1]=10;
+
+
+    }
+}
