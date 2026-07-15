708338be1bdb5a803fbcd7c6979b0d7b29f8ddef
SeuNome <guilhermesanchesloureiro@gmail.com>
2026-07-15 11:28:32 -0300

    Revendo os conceitos de classes

---
+++ src/IntroduçãoMetodos/test/CalculadoraTest1.java
@@ -0,0 +1,11 @@
+package IntroduçãoMetodos.test;
+
+import IntroduçãoMetodos.Dominio.Calculadora;
+
+public class CalculadoraTest1 {
+    public static void main(String[] args) {
+        Calculadora calculadora=new Calculadora();
+        calculadora.multiplicarnumeros(5,5);
+
+    }
+}
