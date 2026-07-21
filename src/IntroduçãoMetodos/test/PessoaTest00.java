2a8d08f92585abea48aada38628c3b8abce71c2f
SeuNome <guilhermesanchesloureiro@gmail.com>
2026-07-20 22:35:20 -0300

    Testanda o codigo sozinho "Treinando"

---
+++ src/IntroduçãoMetodos/test/PessoaTest00.java
@@ -0,0 +1,13 @@
+package IntroduçãoMetodos.test;
+
+import IntroduçãoMetodos.Dominio.Pessoa;
+
+public class PessoaTest00 {
+    public static void main(String[] args) {
+        Pessoa pessoa=new Pessoa();
+        pessoa.setNome("Gabshow");
+        pessoa.setIdade(14);
+        pessoa.imprime();
+
+    }
+}
