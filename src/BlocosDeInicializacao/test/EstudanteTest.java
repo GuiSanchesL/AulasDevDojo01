709568ee9136db24081074fd481ce7ef006ecbcd
SeuNome <guilhermesanchesloureiro@gmail.com>
2026-08-01 13:12:40 -0300

    subimissao de classe dominio

---
+++ src/BlocosDeInicializacao/test/EstudanteTest.java
@@ -0,0 +1,10 @@
+package BlocosDeInicializacao.test;
+
+import BlocosDeInicializacao.Dominio.Estudante;
+
+public class EstudanteTest{
+    public static void main(String[] args) {
+Estudante estudante1 =new Estudante("Guilherme",4);
+        estudante1.imprime();
+    }
+}
