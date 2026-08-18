0c85d16b56d5ce1ad6b04e943d0fed2a7af87882
SeuNome <guilhermesanchesloureiro@gmail.com>
2026-08-18 10:53:53 -0300

    Private

---
+++ src/Associacao/Test/JogadorTest.java
@@ -0,0 +1,17 @@
+package Associacao.Test;
+
+import Associacao.Dominio.Jogador;
+
+public class JogadorTest {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Jogador jogador2 = new Jogador("Romário");
+        Jogador jogador3 = new Jogador("Cafú");
+        Jogador [] jogadores = {jogador1,jogador2,jogador3};
+        for (Jogador jogador : jogadores) {
+            jogador.imprime();
+        }
+
+
+    }
+}
