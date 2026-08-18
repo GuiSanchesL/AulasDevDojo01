39460c9834d9194469c3872ad1a4c03c722507cd
SeuNome <guilhermesanchesloureiro@gmail.com>
2026-08-18 10:52:58 -0300

    Trabalhando um bloco Static com arraylist "Teste"

---
+++ src/ModificadoresStatics/Test/AnimeTest.java
@@ -0,0 +1,11 @@
+package ModificadoresStatics.Test;
+
+import ModificadoresStatics.Dominio.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+    }
+}
