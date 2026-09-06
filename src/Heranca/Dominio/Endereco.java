b3971a9d2a8d0899b310bfd62f34917111f5ecb8
SeuNome <guilhermesanchesloureiro@gmail.com>
2026-09-05 22:46:51 -0300

    Começo de herança.

---
+++ src/Heranca/Dominio/Endereco.java
@@ -0,0 +1,22 @@
+package Heranca.Dominio;
+
+public class Endereco {
+    private int cep;
+    private String rua;
+
+    /*public String getRua(String rua){
+        return rua;
+    }
+        public void setRua(){
+        this.rua=rua;
+    }*/
+
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+}
